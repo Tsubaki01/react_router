25ef2224bee04f1252d4bd3a2b8caa35b5c8fe13
Tsubaki01 <96325598+Tsubaki01@users.noreply.github.com>
2022-06-23 07:44:38 +0900

    Routerの整理

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Page1 } from "./Page1";
 import { Page1DetailA } from "./Page1DetailA";
 import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -18,30 +19,7 @@ export default function App() {
         <Link to="/page2">Page2</Link>
         <br />
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={() => (
-            <Switch>
-              <Route exact path="/page1">
-                <Page1 />
-              </Route>
-              <Route exact path="/page1/detailA">
-                <Page1DetailA />
-              </Route>
-              <Route exact path="/page1/detailB">
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        ></Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
